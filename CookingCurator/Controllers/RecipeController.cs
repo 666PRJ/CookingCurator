1ef05e4b00d11f79fdbe29f705180bce341a3198
hjpatel16 <hjpatel16@myseneca.ca>
2019-01-22 23:48:17 -0500

    Initial Commit

---
+++ CookingCurator/Controllers/RecipeController.cs
@@ -0,0 +1,103 @@
+﻿using CookingCurator.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookingCurator.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private Manager m = new Manager();
+        
+
+        // GET: Recipe/Details/5
+        public ActionResult Details(int? id)
+        {
+            var obj = m.CustomerGetById(id.GetValueOrDefault());
+
+            if (obj == null)
+                return HttpNotFound();
+            else
+                return View(obj);
+        }
+
+        // GET: Recipe/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Recipe/Create
+        [HttpPost]
+        public ActionResult Create(RecipeAddViewModel newItem)
+        {
+            // Validate the input
+            if (!ModelState.IsValid)
+                return View(newItem);
+
+            try
+            {
+                // Process the input
+               
+                var addedItem = m.RecipeAdd(newItem);
+
+                // If the item was not added, return the user to the Create page
+                // otherwise redirect them to the Details page.
+                if (addedItem == null)
+                    return View(newItem);
+                else
+                    return RedirectToAction("Details", new { id = addedItem.recipe_Id });
+            }
+            catch
+            {
+                return View(newItem);
+            }
+        }
+
+        // GET: Recipe/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Recipe/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Recipe/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Recipe/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
