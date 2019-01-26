2b8b3f2570fcbeba96c8e2047739ad707adcc20c
mgquan@myseneca.ca <mgquan@myseneca.ca>
2019-01-25 23:12:22 -0500

    search user now works

---
+++ CookingCurator/EntityModels/UserBaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CookingCurator.EntityModels
+{
+    public class UserBaseViewModel
+    {
+        public int user_ID { get; set; }
+        public string userName { get; set; }
+        public string userEmail { get; set; }
+        public Nullable<int> admin_ID { get; set; }
+    }
+}
