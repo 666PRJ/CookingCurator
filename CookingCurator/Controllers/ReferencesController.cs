aaaab6aaaa8bcd7709bf96c343d8d622df0f51d9
mgquan@myseneca.ca <mgquan@myseneca.ca>
2019-01-31 21:46:34 -0500

    added references page

---
+++ CookingCurator/Controllers/ReferencesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookingCurator.Controllers
+{
+    public class ReferencesController : Controller
+    {
+        private Manager m = new Manager();
+        // GET: References
+        public ActionResult Index()
+        {
+            var t = m.RecipeSourceGetAll();
+            return View(t);
+        }
+
+
+    }
+}
