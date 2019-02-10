f160332be3eab7aefa8481f146a930b6dc4ec7a4
hjpatel16 <hjpatel16@myseneca.ca>
2019-02-09 19:07:00 -0500

    Accept waiver feature added

---
+++ CookingCurator/EntityModels/UserAcceptWaiverViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CookingCurator.EntityModels
+{
+    public class UserAcceptWaiverViewModel
+    {
+        [Key]
+        public int user_ID { get; set; }
+        public bool acceptWaiver { get; set; }
+    }
+}
