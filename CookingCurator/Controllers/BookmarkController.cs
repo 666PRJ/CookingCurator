8ff64e8477b1494918835672de35c85cf05de017
hjpatel16 <hjpatel16@myseneca.ca>
2019-02-23 14:27:28 -0500

    User can now BookMark a Recipe, delete a bookmark

---
+++ CookingCurator/Controllers/BookmarkController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookingCurator.Controllers
+{
+    public class BookmarkController : Controller
+    {
+        private Manager m = new Manager();
+        // GET: Bookmark
+        public ActionResult Index()
+        {
+            return View(m.GetAllBookmarks());
+        }
+
+        // GET: Bookmark/Details/5
+        public ActionResult Details(int iD)
+        {
+            return RedirectToAction("Details", "Recipe", new { id = iD} );
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            bool error =  m.DeleteBookmark(id.GetValueOrDefault());
+            if (error)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Error Deleting Bookmark");
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
