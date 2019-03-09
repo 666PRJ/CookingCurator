6bc3162e93df25b90ea08beef8820bad0b46d8da
hjpatel16 <hjpatel16@myseneca.ca>
2019-03-09 02:06:50 -0500

    Moved ReportRecipeViewModel.cs to EntityModels

--- CookingCurator/EntityModels/ReportRecipeViewModel.cs
+++ CookingCurator/EntityModels/ReportRecipeViewModel.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace CookingCurator.Controllers
+namespace CookingCurator.EntityModels
 {
     public class ReportRecipeViewModel
     {
