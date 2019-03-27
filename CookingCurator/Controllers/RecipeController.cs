6eb20f3392cd6512e5db8a5ae3d178c13c9c5da1
DavidFerri-PRJ <dpferri@myseneca.ca>
2019-03-27 13:44:17 -0400

    Fixed minor bug with diet types when creating or editing recipes

--- CookingCurator/Controllers/RecipeController.cs
+++ CookingCurator/Controllers/RecipeController.cs
@@ -252,13 +252,13 @@ namespace CookingCurator.Controllers
                     for (int i = 0; i < newItem.selectedDietsId.Length; i++)
                     {
                         //None apply cannot be selected with anything else
-                        if (newItem.selectedDietsId[i] == "10")
+                        if (newItem.selectedDietsId[i] == "10" && newItem.selectedDietsId.Length > 1)
                         {
                             compatDiet = false;
                         }
 
                         //Vegan and Vegetarian shouldn't have meat
-                        if (newItem.selectedDietsId[i] == "8" || newItem.selectedDietsId[i] == "5")
+                        else if (newItem.selectedDietsId[i] == "8" || newItem.selectedDietsId[i] == "5")
                         {
                             for (int j = 0; j < newItem.selectedDietsId.Length; j++)
                             {
@@ -354,13 +354,13 @@ namespace CookingCurator.Controllers
                     for (int i = 0; i < newItem.selectedDietsId.Length; i++)
                     {
                         //None apply cannot be selected with anything else
-                        if (newItem.selectedDietsId[i] == "10")
+                        if (newItem.selectedDietsId[i] == "10" && newItem.selectedDietsId.Length > 1)
                         {
                             compatDiet = false;
                         }
 
                         //Vegan and Vegetarian shouldn't have meat
-                        if (newItem.selectedDietsId[i] == "8" || newItem.selectedDietsId[i] == "5")
+                        else if (newItem.selectedDietsId[i] == "8" || newItem.selectedDietsId[i] == "5")
                         {
                             for (int j = 0; j < newItem.selectedDietsId.Length; j++)
                             {
@@ -512,9 +512,20 @@ namespace CookingCurator.Controllers
                 {
                     for (int i = 0; i < recipes.selectedDietsId.Length; i++)
                     {
-                        if (recipes.selectedDietsId[i] == "10")
+                        if (recipes.selectedDietsId[i] == "10" && recipes.selectedDietsId.Length > 1)
                         {
                             compatDiet = false;
+                        }
+
+                        else if (recipes.selectedDietsId[i] == "8" || recipes.selectedDietsId[i] == "5")
+                        {
+                            for (int j = 0; j < recipes.selectedDietsId.Length; j++)
+                            {
+                                if (recipes.selectedDietsId[j] != "6" && recipes.selectedDietsId[j] != "5" && recipes.selectedDietsId[j] != "8")
+                                {
+                                    compatDiet = false;
+                                }
+                            }
                         }
                     }
 
