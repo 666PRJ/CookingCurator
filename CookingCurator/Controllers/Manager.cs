04754a1b9a00eabfbf27d8d45a8e0c3d09b9392f
mgquan@myseneca.ca <mgquan@myseneca.ca>
2019-04-02 21:32:34 -0400

    another update

--- CookingCurator/Controllers/Manager.cs
+++ CookingCurator/Controllers/Manager.cs
@@ -324,28 +324,8 @@ namespace CookingCurator.Controllers
 
             user.userName = newUsername.userName;
             ds.Entry(user).State = System.Data.Entity.EntityState.Modified;
-            try
-            {
-                ds.SaveChanges();
-            }
-            catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
-            {
-                Exception exception = dbEx;
-                foreach (var validationErrors in dbEx.EntityValidationErrors)
-                {
-                    foreach (var validationError in validationErrors.ValidationErrors)
-                    {
-                        string message = string.Format("{0}:{1}",
-                            validationErrors.Entry.Entity.ToString(),
-                            validationError.ErrorMessage);
 
-                        //create a new exception inserting the current one
-                        //as the InnerException
-                        exception = new InvalidOperationException(message, exception);
-                    }
-                }
-                throw exception;
-            }
+            ds.SaveChanges();
 
             FormsAuthentication.SignOut();
             FormsAuthentication.SetAuthCookie(newUsername.userName, false);
